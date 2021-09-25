2f859d2e21877062ef8aa854c90e8e9d848bdc98
bhatiagagan24 <bhatiagagan24@gmail.com>
2021-09-25 21:37:22 +0530

    carousel removed from home screen and replaced by list of network images

--- lib/screens/appLandingPage.dart
+++ lib/screens/appLandingPage.dart
@@ -4,11 +4,18 @@
 
 // import 'dart:ffi';
 
+import 'dart:convert';
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:http/http.dart' as http;
+
+class HomeScreenPhotos {
+  var photo_name;
+  var id;
+  HomeScreenPhotos({this.photo_name, this.id});
+}
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -18,18 +25,36 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  @override
-  void initState() {
-    print('Inside initstate');
-    log("inside initstate");
-  }
+  var no_of_images = 0;
+  var image_list = [];
 
-  Future<String> getImageUri() async {
+  Future<List<HomeScreenPhotos>> getImageUri() async {
+    List<HomeScreenPhotos> name_list = [];
+    var new_name_list = [];
     print('inside future function');
     final url = Uri.parse('http://192.168.1.10:5000/urlfetch/getlinks');
     http.Response response = await http.get(url);
     print(response.body);
-    return response.body;
+    var respdata = json.decode(response.body);
+    print(respdata);
+
+    for (var i = 0; i < respdata.length; i++) {
+      print("inside for loop");
+      print(respdata[i]['imageName']);
+      print(respdata[i]);
+      HomeScreenPhotos temp1 =
+          new HomeScreenPhotos(photo_name: respdata[i]['imageName'], id: i);
+      name_list.add(temp1);
+      new_name_list.add(respdata[i]['imageName']);
+    }
+    setState() {
+      no_of_images = respdata.length;
+      image_list = new_name_list;
+    }
+
+    print("named list -> ");
+    print(name_list[0]);
+    return name_list;
   }
 
   @override
@@ -87,27 +112,57 @@ class _LoginScreenState extends State<LoginScreen> {
         ),
 
         // HomePage banner will be added here now: -
-        CarouselSlider(
-          items: [
-            Card(
-              child: Image.network(
-                  'https://4.imimg.com/data4/WA/YP/GLADMIN-14324829/touch-screen-500x500.png'),
-            ),
-          ],
-          options: CarouselOptions(
-            height: 200,
-            autoPlay: true,
-            enableInfiniteScroll: true,
-          ),
-        ),
+
+        // ignore: avoid_unnecessary_containers
+        // SizedBox(
+        //   child: FutureBuilder(
+        //     builder:(BuildContext ctx, AsyncSnapshot snapshot) {
+        //       if
+        //     }),
+        // ),
+
+        // CarouselSlider(
+        //   items: [
+        //     // for(var i=0; i<)
+        //     Card(
+        //       child: Image.network(
+        //           'https://4.imimg.com/data4/WA/YP/GLADMIN-14324829/touch-screen-500x500.png'),
+        //     ),
+        //   ],
+        //   options: CarouselOptions(
+        //     height: 200,
+        //     autoPlay: true,
+        //     enableInfiniteScroll: true,
+        //   ),
+        // ),
         FutureBuilder(
           future: getImageUri(),
           builder: (context, AsyncSnapshot snapshot) {
-            if (snapshot.hasData) {
-              log(snapshot.data);
-              return CircularProgressIndicator();
+            if (snapshot.hasData != true) {
+              // log(snapshot);
+              log("data was not there in the snapshot");
+              return Container(child: CircularProgressIndicator());
             } else {
-              return CircularProgressIndicator();
+              return ListView.builder(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                itemCount: snapshot.data.length,
+                itemBuilder: (ctx, index) => Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    // Text(snapshot.data[index].photo_name),
+                    Container(
+                      height: 200,
+                      width: screen_width,
+                      child: Card(
+                        child: Image.network(
+                            "http://192.168.1.10:5000/static/media/" +
+                                snapshot.data[index].photo_name),
+                      ),
+                    )
+                  ],
+                ),
+              );
             }
           },
         ),
@@ -119,15 +174,6 @@ class _LoginScreenState extends State<LoginScreen> {
         SingleChildScrollView(
           child: Column(
             children: [
-              // ElevatedButton(onPressed: () {}, child: Text('yo')),
-              // ButtonTheme(
-              //   minWidth: 200,
-              //   height: 400,
-              //   child: ElevatedButton.icon(
-              //     onPressed: () {},
-              //     icon: Icon(Icons.help),
-              //     label: Text('Help'),
-              //   ),
               ButtonBar(
                 alignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
@@ -149,7 +195,6 @@ class _LoginScreenState extends State<LoginScreen> {
                   ),
                 ],
               ),
-
               ButtonBar(
                 alignment: MainAxisAlignment.center,
                 children: <Widget>[
@@ -171,7 +216,6 @@ class _LoginScreenState extends State<LoginScreen> {
                   ),
                 ],
               ),
-
               ButtonBar(
                 alignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
