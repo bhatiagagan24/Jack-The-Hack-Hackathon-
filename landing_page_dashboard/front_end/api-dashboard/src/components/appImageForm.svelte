4034d2b2bfd217ae1b172d30dd431a32adb5b157
bhatiagagan24 <bhatiagagan24@gmail.com>
2021-09-26 11:23:33 +0530

    file upload web component corrected

--- landing_page_dashboard/front_end/api-dashboard/src/components/appImageForm.svelte
+++ landing_page_dashboard/front_end/api-dashboard/src/components/appImageForm.svelte
@@ -2,13 +2,27 @@
 
 <script>
     // var image_name = "";
+    import Button, {Label} from '@smui/button';
     let files;
     $: files
+    // function for sending the form data to the API(endpoint -> /upload)
+    async function sendingImages() {
+        let formData = new FormData()
+        formData.append("__file_to_be_uploaded", files[0])
+        const resp = await fetch('http://192.168.1.10:5000/upload', {
+            method: 'POST',
+            body: formData,
+            
+        });
+        const resp1 = await resp
+        console.dir(resp1);
+    }
+
 </script>
 
 <main>
     <form>
-       <input type="file" bind:files />
+       <input type="file" bind:files required/>
     <h4>
         File Name
         {#if files}
@@ -17,6 +31,6 @@
         </p>
         {/if}
     </h4>
-    <input type="submit" />
+    <Button type="button" on:click="{sendingImages}"> <Label>This is the button</Label></Button>
 </form>
 </main>
