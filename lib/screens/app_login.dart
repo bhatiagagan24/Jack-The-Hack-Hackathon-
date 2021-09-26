a0c39431088e9ef49b856fdebbc9609e31f522c4
Luciferxx <vatsaltulshyan@gmail.com>
2021-09-26 23:10:06 +0530

    Added user db API

--- lib/screens/app_login.dart
+++ lib/screens/app_login.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:smart_journey_experience/API/google_signin_api.dart';
+import 'package:http/http.dart';
 
 class SignIn extends StatelessWidget {
   const SignIn({Key? key}) : super(key: key);
@@ -26,6 +27,17 @@ class SignIn extends StatelessWidget {
     );
   }
 
+  Future<void> send_signedIn_user_data(var user_name, var user_email) async {
+    Map<String, String> queryParams = {
+      "user_name": "$user_name",
+      "user_email": "$user_email"
+    };
+    String queryString = Uri(queryParameters: queryParams).query;
+    var requesturl = 'http://192.168.1.22:5000/userdatabase?' + queryString;
+    var uri = Uri.parse(requesturl);
+    Response response = await get(uri);
+  }
+
   Future signIn(BuildContext context) async {
     final user = await GoogleSignInApi.login();
     if (user == null) {
@@ -35,6 +47,7 @@ class SignIn extends StatelessWidget {
       //user.displayName
       ScaffoldMessenger.of(context)
           .showSnackBar(SnackBar(content: Text('Welcome' + user.displayName!)));
+      send_signedIn_user_data(user.displayName, user.email);
       // Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=> LoggedInPage(user:User)));
     }
   }
